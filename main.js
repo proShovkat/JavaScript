0fdb961d464057041da9b41b0e784117171c1baf
proShovkat <104549731+proShovkat@users.noreply.github.com>
2022-05-12 12:26:07 +0500

    Javani boshlash

---
+++ main.js
@@ -0,0 +1,29 @@
+var a = prompt( 'Ismingizni kiriting')
+var b = prompt ('Yoshingizni kiriting')
+console.log("Ismi: " + a, "Yoshi: " + b)
+
+
+var c = alert('Qoshish amali ishtirok etgan misolni kiriting')
+var d = +prompt('Birinchi sonni kiriting') // '55'
+var e = +prompt('ikkinchi sonni kiriting') // '11'
+console.log('sizning javob: '+(d+e))
+
+
+var f =alert('ayirish amalini kiriting')
+var g =+prompt('Birinchi sonni kiriting')
+var h =+prompt('Ikkinchi sonni kiriting')
+console.log('sizning javob: '+(g-h ))
+
+
+var i = alert("Ko'paytirish amalini kiriting")
+var j =+prompt("Birinchi sonni kiriting")
+var k =+prompt("Ikkinchi sonni kiriting")
+console.log("sizning javob:"+(j*k))
+
+var l = alert("Bo'lish amalini kiriting")
+var m =+prompt("Birinchi sonni kiriting")
+var n =+prompt("Ikkinchi sonni kiriting")
+console.log("Sizning javob:"+(m/n))
+
+
+
